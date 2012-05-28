6f00665edf3fed063a664b4651ae429649a88e78
Martin Gontovnikas <gonto@zauberlabs.com>
2012-05-28 12:40:38 -0300

    Rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AComerla::Application.config.secret_token = '5c865b9a5428138ee0dd8cd3db071214ddcb4dfb1e5b3555517642ebbf9e9a554b3c886f0e613891de4e0477507bbef8a76f7805487832d5c36a6fb0ea18d415'
