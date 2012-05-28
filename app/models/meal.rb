cba5006751ca0d64121451666dfa75c6e2985304
Hernan Zalazar <hernan@zaubersoftware.com>
2012-05-28 15:10:23 -0300

    True Scaffold instead of activeadmin scaffold. Validation to model.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,6 @@
 class Meal < ActiveRecord::Base
   belongs_to :restaurant
   attr_accessible :description, :name, :price, :restaurant_id
+  validates :description, :name, :price, :restaurant_id, :presence => true
+  validates :price, :numericality => { :greater_than => 0 }
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ActiveRecord::Base
+  validates :address, :phone, :name, :presence => true
+  validates :phone, :numericality => { :only_integer => true }
   attr_accessible :address, :name, :phone
 end
